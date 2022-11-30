e9ecb253242041fd9600d23ffdc8a463ed095304
Oliver Palomera Palacios <oliverpalomerapal@gmail.com>
2022-11-29 18:50:21 -0600

    added changes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const usuariosRoutes = require('./src/routes/usuarios');
+const inventarioRoutes = require('./src/routes/inventario');
+const productosRoutes = require('./src/routes/productos');
+const comprasRoutes = require('./src/routes/compras');
 const mongoose = require('mongoose');
 
 const apiRoutes = require('./routes');
@@ -12,8 +17,12 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.json());
-app.use(apiRoutes);
+app.use('/usuarios', usuariosRoutes);
+app.use('/inventario', inventarioRoutes);
+app.use('/productos', productosRoutes);
+app.use('/compras', comprasRoutes);
 
+app.use('/usuarios', usuario)
 const uri = 'mongodb+srv://twofold:twofold12345@proyectointegrador.ib2xxwk.mongodb.net/ProyectoIntegrador'
 
 mongoose.connect(uri, (err) => {
